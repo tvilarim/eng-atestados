e4594e2448d10b200cd33bd0f881cce4103275fc
tvilarim <tvilarim@msn.com>
2024-08-20 22:54:32 -0300

    v0.1.6 Novos testes para gerar tabela de serviços

--- flask_app_report/app.py
+++ flask_app_report/app.py
@@ -330,5 +330,5 @@ def index():
                            service_table=service_table)
 
 # Inicia a aplicação Flask
-if __name == '__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
